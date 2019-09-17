48b8603502ab3edd4b66e56157209dc2d720616a
Scott-Tan <sctan6688@gmail.com>
2019-09-17 13:21:57 -0500

    testing trigger

--- src/main/java/com/geekcap/vmturbo/Thing.java
+++ src/main/java/com/geekcap/vmturbo/Thing.java
@@ -2,7 +2,8 @@ package com.geekcap.vmturbo;
 
 public class Thing {
   private int n = 0;
-
+  private int m = 1;
+  
   public int getN() {
     return n;
   }
@@ -10,4 +11,13 @@ public class Thing {
   public void setN(int n) {
     this.n = n;
   }
+  
+  public int getM() {
+    return m;
+  }
+
+  public void setM(int m) {
+    this.m = m;
+  }
+  
 }
